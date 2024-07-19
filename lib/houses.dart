6a4288112d8f7cec132188e378c89bbb81688545
jehovah-son <chukwukasuccess0@gmail.com>
2024-07-18 18:05:39 -0700

    renamed:    assets/PHOTO-2024-07-02-09-27-41.jpg -> assets/img1.jpg 	renamed:    assets/PHOTO-2024-07-09-06-53-23.jpg -> assets/img2.jpg 	renamed:    assets/PHOTO-2024-07-09-06-57-05.jpg -> assets/img3.jpg 	renamed:    assets/PHOTO-2024-07-11-06-40-16.jpg -> assets/img4.jpg 	renamed:    assets/PHOTO-2024-07-15-04-45-36.jpg -> assets/img5.jpg 	renamed:    assets/PHOTO-2024-07-16-06-29-17.jpg -> assets/img6.jpg 	modified:   lib/houses.dart 	modified:   lib/my_home.dart

--- lib/houses.dart
+++ lib/houses.dart
@@ -1,53 +1,81 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(const MaterialApp(
-    home:Houses( ),
+    home: Houses(),
   ));
 }
-
-
 
 class Houses extends StatelessWidget {
   const Houses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-     appBar: AppBar(
-    
-        backgroundColor:Colors.lightBlue,
-        
-        title:const Text('houses for sale'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: const Text('gadget currently available'),
       ),
-      body:   Container(
-       // color: Colors.black,
-      child: Column(children: [
-          Image.asset('assets/pic1.jpg')
-        ],
-    ),
-        
+      body: Container(
+        // color: Colors.black,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+                child: Column(
+              children: [
+                Image.asset(
+                  'assets/pic1.jpg',
+                  height: 500,
+                ),
+                Text('samsung galaxy s21=200,000')
+              ],
+            )),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/img1.jpg',
+                    height: 500,
+                  ),
+                  Text('Hp elitebook 820 g2 8gb/256gb = 220,000')
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/img3.jpg',
+                    height: 500,
+                  ),
+                  Text('playstation4 with 2 pads & 10 games = 300,000')
+                ],
+              ),
+            ),
+            Container(
+                child: Column(
+              children: [
+                Image.asset(
+                  'assets/img4.jpg',
+                  height: 500,
+                ),
+                Text('samsung galaxys9 = 165,000'),
+              ],
+            )),
+            Container(
+                child: Column(
+              children: [
+                Image.asset(
+                  'assets/img6.jpg',
+                  height: 500,
+                ),
+                Text('ipad pro 2015 128gb = 310,000')
+              ],
+            )),
+          ],
+        ),
       ),
-  
     );
-  }
-}
-
-
-
-
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
--- lib/my_home.dart
+++ lib/my_home.dart
@@ -1,72 +1,118 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:practice/houses.dart';
-
-
 
 class Home extends StatelessWidget {
   const Home({super.key});
-  
+
   get child => null;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(      // it provide with a base layout for our application with the option to hv app bar at the top for a tittle an icon ...
-     backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        title:const Text('max gagdet', style: TextStyle(color: Colors.white, )),
-        leading: Image.asset('assets/img.png'),
-        backgroundColor: Colors.brown[500],
-
-      ),
+    return Scaffold(
+        // it provide with a base layout for our application with the option to hv app bar at the top for a tittle an icon ...
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: const Text('Max gadget',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+          leading: Image.asset('assets/img.png'),
+          backgroundColor: Colors.brown[500],
+          actions: const [
+            Icon(
+              Icons.shopping_cart,
+              size: 50,
+            ),
+            Icon(
+              Icons.menu,
+              size: 50,
+            ),
+          ],
+        ),
 // write your body content under the app bar
-body:Container(
-  margin: const EdgeInsets.all(20),
-  alignment: Alignment.topCenter,
-child:  Column(
-
-children:<Widget>[ 
-  const Icon(Icons.person,
-   size: 150,),
-  const Text('Name:chukwuka-success' ,style:TextStyle(fontStyle: FontStyle.italic,
-  decoration: TextDecoration.underline,fontSize:20, ) ,),
-  const Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Column( children: [
-Icon(Icons.email,
-size: 150,),
-Text('chukwukasuccess0@gmail.com' , style: TextStyle(fontStyle:FontStyle.italic,fontSize: 20, ),)
-]),
-
-Column(children: [
-  Icon(Icons.access_alarm,
-  size: 150,),
-  Text('08118477430', style: TextStyle(fontSize: 20),),
-],)
-
-],
-    
-    
-
-    
-    ),
-
- ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Colors.brown[800]),
-  onPressed:() {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Houses()),
-            );
-          }, child: const Text('press me', style: TextStyle( color: Colors.lightBlue),)),
-
-],
- ),
-
-
-
-)
-    );
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Welcome to MAX GADGET Phone Store',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.underline,
+                  fontSize: 20,
+                ),
+              ),
+              const Text(
+                'At Phone Store Name, we pride ourselves on being your ultimate destination for all things mobile.Step into our phone store and enter a world of endless possibilities. Discover the latest in mobile technology, where sleek design meets cutting-edge innovation.  at Max gadget we make sure we give the best deal,are satisfy a customers needs',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20,
+                ),
+              ),
+              const Text(
+                'hit the button below to see list of available gadget and there prices',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.underline,
+                  fontSize: 30,
+                ),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.brown[800]),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Houses()),
+                    );
+                  },
+                  child: const Text(
+                    'price list',
+                    style: TextStyle(color: Colors.lightBlue),
+                  )),
+              const Text(
+                'CONTACT US ',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.underline,
+                  fontSize: 25,
+                ),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(children: [
+                    Icon(
+                      Icons.email,
+                      size: 50,
+                    ),
+                    Text(
+                      'chukwukasuccess0@gmail.com',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 20,
+                      ),
+                    )
+                  ]),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.phone_android,
+                        size: 50,
+                      ),
+                      Text(
+                        '08118477430',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
 
